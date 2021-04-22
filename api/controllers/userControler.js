7e88a5ef5b786310d0a851fb31e2d3ba6a9b9c01
frida stenberg <fridastenberg97@hotmail.com>
2021-04-22 10:17:54 +0200

    retrieved data(users) from my Db

---
+++ api/controllers/userControler.js
@@ -0,0 +1,22 @@
+const sqlite3 = require('sqlite3');
+const path = require('path');
+const util = require('util');
+
+const db = new sqlite3.Database(path.join(__dirname, '../myDb.db'));
+
+db.all = util.promisify(db.all); //for get all users
+
+const getAllUsers = async (reg, res) => {
+    let query = `SELECT * FROM users`;
+    db.all(query)
+        .then((users) => {
+            res.json(users);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+};
+
+module.exports = {
+    getAllUsers
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const port = 3001;
 const channelRoutes = require('./routes/channelRoutes.js');
+const userRoutes = require('./routes/userRoutes');
 
 //server setup
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json())
 
 //middleware for checking, get accsess to all routes
 app.use('/api/v1/channels', channelRoutes);
+app.use('/api/v1/users', userRoutes);
 
 //starts the server
 app.listen(port, (err) => {
