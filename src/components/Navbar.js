3f6819fc78da095648cdfcc2300015dff7448bfd
frida stenberg <fridastenberg97@hotmail.com>
2021-04-23 11:37:44 +0200

    fixed a template for navbar, need some more fixes in future

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,21 @@ import * as ReactBootstrap from "react-bootstrap";
 
 const Navbar = () => {
     return(
-        <div className="Home">
-            <h1>Lyssna på dina favorit Kanaler</h1>
-        </div>
+        <ReactBootstrap.Navbar bg="light" expand="lg">
+            <ReactBootstrap.Navbar.Brand as={Link} to="/">Home</ReactBootstrap.Navbar.Brand>
+            <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
+                <ReactBootstrap.Nav className="mr-auto">
+                <ReactBootstrap.Nav.Link href="#home">sida 2</ReactBootstrap.Nav.Link>
+                <ReactBootstrap.Nav.Link href="#link">sida 3</ReactBootstrap.Nav.Link>
+                <ReactBootstrap.Nav.Link href="#link">Logga in</ReactBootstrap.Nav.Link>
+                </ReactBootstrap.Nav>
+                <ReactBootstrap.Form inline>
+                <ReactBootstrap.FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <ReactBootstrap.Button variant="outline-success">Search</ReactBootstrap.Button>
+                </ReactBootstrap.Form>
+            </ReactBootstrap.Navbar.Collapse>
+</ReactBootstrap.Navbar>
     )
 };
 
